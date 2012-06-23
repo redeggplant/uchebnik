378975da6d6066a6f914e54012f6d0c5c4b7fc0d
Mike Pylaev <regplant@gmail.com>
2012-06-23 14:29:54 +0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UchebnikNew::Application.config.secret_token = '319d964536dbb7abafc5ccb9f76e326818fb1becf6f494f71a83fda9c6499a180d70c48bf09a977a6eb9623162e77d12d9fd78d56c7a47eb6b9958f686b7017c'
